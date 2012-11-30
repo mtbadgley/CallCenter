f976acdf9a1379ce5d4ae1ad48c1803103ccc9c3
Matt <mtbadgley@gmail.com>
2012-11-30 12:47:55 -0500

    Initial Checkin

---
+++ HelloTFS/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloTFS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello TFS, now with proxy support!. This is another change. Isn't this great! How does this work.");
+        }
+    }
+}
